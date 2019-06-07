da0cec4943eb0a4974d4c85e24838f00bc7eee19
Carles del Río Francés <cdelriofrances@Carless-iMac.local>
2019-06-07 23:00:24 +0200

    first commit

--- src/Counter.js
+++ src/Counter.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
 
 export default class Counter extends Component {
+  state = {
+    count: 0
+  }
+
+  addTen = () => {
+    this.setState({
+      count: this.state.count + 10
+    })
+  }
+  takeFive = () => {
+    this.setState({
+      count: this.state.count - 5
+    })
+  }
+  resetCount = () => {
+    this.setState({
+      count: this.state.count * 0
+    })
+  }
+
   render() {
+    const { count } = this.state
     return (
       <section className="Counter">
-        <h1>Count: {0}</h1>
-        <button onClick={() => {}} className="full-width">Increment</button>
-        <button onClick={() => {}} className="full-width">Decrement</button>
-        <button onClick={() => {}} className="full-width">Reset</button>
+        <h1>Count: {count}</h1>
+        <button onClick={this.addTen} className="full-width">Increment by 10</button>
+        <button onClick={this.takeFive} className="full-width">Decrement by 5</button>
+        <button onClick={this.resetCount} className="full-width">Reset</button>
       </section>
     );
   }
